6380df245f604ee87908d15e1cdf6a6862abeb46
Mai Hoang <mnhoang.80@gmail.com>
2017-09-27 17:11:32 -0700

    Add weather-list

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Search from '../containers/search';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Search />
+        <WeatherList />
       </div>
     );
   }
